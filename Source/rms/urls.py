13e011099ae22548792d9edcbd4a3a34b99ac410
TBS5596 <thomsimbeye99@gmail.com>
2023-03-30 11:54:01 +0200

    v3 urls

--- Source/rms/urls.py
+++ Source/rms/urls.py
@@ -22,7 +22,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
-    path('portal/', include('portal.urls')),
+    path('', include('portal.urls')),
 ]
 
 if settings.DEBUG:
